36b7242c2e734234f445c1f7272902b798013eab
Marcin <Marcin@DESKTOP-L62L7NR>
2023-04-08 09:28:56 +0200

    Add db context class

---
+++ TeacherApp/Models/TeacherAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeacherApp.Models
+{
+    public class TeacherAppDbContext : DbContext
+    {
+        public TeacherAppDbContext(DbContextOptions<TeacherAppDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+    }
+}
